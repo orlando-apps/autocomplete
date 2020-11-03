37c6479aa194f82ed1174f774c748f7bc416dcd9
Orlando <jcsf1991@gmail.com>
2020-11-02 18:06:49 -0800

    remove bundle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,10 @@ class App extends React.Component {
         this.setState({
           options: result
         });
-      });
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
   }
 
   render (){
